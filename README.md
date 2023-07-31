19eec82653b73a6b1c1141cb36c77eef95edec0b
Ross Murray <rossmurray@protonmail.com>
2023-07-31 13:22:40 +1000

    feat: impl. fairly basic script

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # ChatBot for Local Markdown
 
+## Setup
+
+1. `brew install python`
+1. `python3 -m venv my_env`
+1. `source my_env/bin/activate`
+1. `pip3 install -r requirements.txt`
+1. Replace {KEY} with your OPENAI API Key, and run `echo 'OPENAI_API_KEY={KEY}' > .env`
+1. Replace {PATH} with your local filepath to the obsidian vault, then run `echo 'OBSIDIAN_VAULT_PATH={PATH}' >> .env`
+
 ## Usage
+
+From the root of this repo:
+
+1. [once per session] activate the python environment `source my_env/bin/activate`
+
+Then to ask a question:
+
+1. `python3 run.py "{question}"`
---
+++ run.py
@@ -0,0 +1,31 @@
+from llama_index import VectorStoreIndex, SimpleDirectoryReader, download_loader, StorageContext, load_index_from_storage
+from dotenv import load_dotenv
+import argparse
+import openai
+import os
+import sys
+
+load_dotenv()
+
+parser = argparse.ArgumentParser(
+    prog='Document Chat Bot',
+    description='A chatbot for your documents')
+parser.add_argument('question')
+args = parser.parse_args()
+
+openai.api_key = os.environ['OPENAI_API_KEY']
+
+ObsidianReader = download_loader('ObsidianReader')
+documents = ObsidianReader('/Users/rossmurray/Documents/rei-obsidian-vault').load_data()
+
+
+if os.path.exists('./storage'):
+    storage_context = StorageContext.from_defaults(persist_dir="./storage")
+    index = load_index_from_storage(storage_context)
+else:
+    index = VectorStoreIndex.from_documents(documents)
+    index.storage_context.persist()
+
+query_engine = index.as_query_engine()
+response = query_engine.query(args.question)
+print(response)
