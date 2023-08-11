2cdf65df65d7b9434b61cfa02d460e2ecd887cb2
Ross Murray <ross.murray@gosource.com.au>
2023-08-11 17:50:12 +1000

    feat: langchain + UI improvements and debug (#2)
    * ref: split out nodes from documents to handle both separately
    
    * feat: add basic debugging
    
    * feat: add documents to debug output
    
    * feat: looking to move to langchain
    
    * feat: impl base langchain function
    
    * feat: redesign tui to allow for settings / debugging
    
    * feat: init debug output capability to separate tab on submit
    
    ---------
    
    Co-authored-by: Ross Murray <rossmurray@protonmail.com>

--- src/quandary/app/utils.py
+++ src/quandary/app/utils.py
@@ -1,8 +1,33 @@
 
-from llama_index import VectorStoreIndex, download_loader, StorageContext, load_index_from_storage
+from llama_index import VectorStoreIndex, download_loader, StorageContext, load_index_from_storage, Response
+from llama_index.schema import MetadataMode
+from llama_index.node_parser import SimpleNodeParser
 from dotenv import load_dotenv
 import openai
 import os
+from langchain.llms import OpenAI
+from langchain.chat_models import ChatOpenAI
+from langchain.schema import HumanMessage
+
+
+def run_lang_quandary(question):
+
+    load_dotenv()
+    # Constants    
+    OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
+    QNDY_DOCS_PATH = os.environ['QNDY_DOCS_PATH']
+    QNDY_INDEX_PATH = os.environ['QNDY_INDEX_PATH']
+    QNDY_DEBUG = os.environ.get('QNDY_DEBUG', "False") in ["True", "1", "true"]
+    openai.api_key = OPENAI_API_KEY
+
+    llm = OpenAI()
+    chat_model = ChatOpenAI()
+
+    messages = [HumanMessage(content=question)]
+    prediction = chat_model.predict_messages(messages)
+    return Response(prediction.content)
+
+
 
 def run_quandary(question):
 
@@ -13,10 +38,15 @@ def run_quandary(question):
     OPENAI_API_KEY = os.environ['OPENAI_API_KEY']
     QNDY_DOCS_PATH = os.environ['QNDY_DOCS_PATH']
     QNDY_INDEX_PATH = os.environ['QNDY_INDEX_PATH']
+    QNDY_DEBUG = os.environ.get('QNDY_DEBUG', "False") in ["True", "1", "true"]
     openai.api_key = OPENAI_API_KEY
 
     reader = download_loader('ObsidianReader')
     documents = reader(QNDY_DOCS_PATH).load_data()
+   
+    # parse nodes from documents
+    parser = SimpleNodeParser()
+    nodes = parser.get_nodes_from_documents(documents)
 
     if os.path.exists(QNDY_INDEX_PATH):
         # index already exists, so load it in
@@ -24,10 +54,25 @@ def run_quandary(question):
         index = load_index_from_storage(storage_context)
     else:
         # create index and persist it
-        index = VectorStoreIndex.from_documents(documents)
+        index = VectorStoreIndex(nodes)
         index.storage_context.persist()
 
     query_engine = index.as_query_engine()
 
-    response = query_engine.query(question)
+    if QNDY_DEBUG:
+        debug_output = f"""
+```
+Debug: Enabled
+Documents: {len(documents)}
+Nodes: {len(nodes)}
+```
+        """
+        original_response = query_engine.query(question)
+        response = Response(
+            response = original_response.response + "\n\n" + debug_output,
+            source_nodes = original_response.source_nodes,
+            metadata = original_response.metadata,
+        )
+    else:
+        response = query_engine.query(question)
     return response
--- src/quandary/ui/components.py
+++ src/quandary/ui/components.py
@@ -1,30 +1,10 @@
 from textual.app import ComposeResult
-from textual.containers import ScrollableContainer
-from textual.widgets import Input, Static, Markdown
+from textual.containers import ScrollableContainer, VerticalScroll, Container
+from textual.widgets import Input, Static, Markdown, TabbedContent, Placeholder, Header, Footer, Pretty
 from textual.reactive import reactive
+from textual.screen import Screen
 
-from quandary.app.utils import run_quandary
-
-class InputPane(Static):
-
-    input_text = reactive("")
-
-    """A widget to accept and send input"""
-    def compose(self) -> ComposeResult:
-        """child widgets of an InputPane"""
-        yield Input(placeholder="What do you want to know?")
-
-    def on_input_changed(self, event: Input.Changed) -> None:
-        self.input_text = event.input.value
-
-    def clear_input(self) -> None:
-        """Updates the input field"""
-        self.query_one(Input).value = ""
-
-    def submit_question(self, question):
-        """submits a question to quandary"""
-        answer = run_quandary(question)
-        return answer
+from quandary.app.utils import run_quandary, run_lang_quandary
 
 class ResponsePane(Static):
     """A widget to view results."""
@@ -46,17 +26,142 @@ class ResponsePane(Static):
 
 class QandAPane(Static):
     """A panel for submitting questions and reading answers"""
-
     def compose(self) -> ComposeResult:
         yield ScrollableContainer(ResponsePane(), InputPane())
+
+class DebugPane(Static):
+    """a widget to view debug outputs."""
+    debug_data = [{ 
+        "key": "value",
+        "int": 123
+    }]
+    def compose(self) -> ComposeResult:
+        yield Pretty(self.debug_data)
+
+    def clear_debug_pane(self) -> None:
+        """clears the debug pane."""
+        self.query_one(Pretty).update("")
+
+    def append_debug_pane(self, content) -> None:
+        """appends content to the debug pane."""
+        self.debug_data.append(content)
+        self.query_one(Pretty).update(self.debug_data)
+
+class TabbedNavigation(TabbedContent):
+    """The navigation pane"""
+    def compose(self) -> ComposeResult:
+        with TabbedContent("Response", "Debug", "Settings"):
+            yield ResponsePane()
+            yield DebugPane()
+            yield Placeholder()
+
+class InputPane(Static):
+    """A widget to accept and send input"""
+    DEFAULT_CSS = """
+    InputPane {
+        height: 1fr;
+        width: 1fr;
+    }
+    """
+    def compose(self) -> ComposeResult:
+        """child widgets of an InputPane"""
+        yield Input(id="text_input_field", placeholder="What do you want to know?")
+
+class BorderTop(Static):
+    """A border for the top of the application."""
+    DEFAULT_CSS = """
+    BorderTop {
+        height: 1;
+        dock: top;
+    }
+    """
+    def compose(self) -> ComposeResult:
+        yield Header()
+
+class BorderBottom(Static):
+    """A border for the bottom of the application."""
+    DEFAULT_CSS = """
+    BorderBottom {
+        height: 1;
+        dock: bottom;
+    }
+    """
+    def compose(self) -> ComposeResult:
+        yield Footer()
+
+class ScreenBody(Static):
+    """The body content for the main screen."""
+    DEFAULT_CSS = """
+    ScreenBody {
+        width: 1fr;
+        height: 1fr;
+        border: solid white;
+        padding: 1;
+    }
+
+    #screen_body_tab_nav {
+        dock: top;
+        height: 1fr;
+    }
+
+    #screen_body_top_v_scroll {
+        dock: top;
+        height: 1fr;
+    }
+
+    #screen_body_bottom_v_scroll {
+        dock: bottom;
+        height: 3;
+    }
+
+    #screen_body_input_pane {
+        dock: top;
+    }
+    """
+
+    input_text = reactive("")
+    def on_input_changed(self, event: Input.Changed) -> None:
+        self.input_text = event.input.value
 
     def on_input_submitted(self, event: Input.Submitted) -> None:
         input_pane = self.query_one(InputPane)
         response_pane = self.query_one(ResponsePane)
+        debug_pane = self.query_one(DebugPane)
         response_pane.clear_markdown()
+        # update the debug pane with new prompt
+        debug_pane.append_debug_pane(event.input)
         # submit prompt
-        answer = input_pane.submit_question(event.input.value).response
+        answer = self.submit_question(event.input.value)
         # clear input
-        self.query_one(InputPane).clear_input()
+        self.clear_input()
+        # update the debug pane with answer
+        debug_pane.append_debug_pane(answer)
         # display answer
-        response_pane.update_markdown(answer)
+        response_pane.update_markdown(answer.response)
+
+    def clear_input(self) -> None:
+        """resets the input field"""
+        self.query_one("#text_input_field", Input).value = ""
+
+    def submit_question(self, question):
+        """submits a question to quandary"""
+        answer = run_lang_quandary(question)
+        return answer
+
+    def compose(self) -> ComposeResult:
+        yield Container(
+            VerticalScroll(
+                TabbedNavigation(id="screen_body_tab_nav"),
+                id="screen_body_top_v_scroll"),
+            VerticalScroll(
+                InputPane(id="screen_body_input_pane"),
+                id="screen_body_bottom_v_scroll"),
+            id="screen_body_outer_container")
+
+        
+class MainScreen(Screen):
+    """The primary display screen for quandary."""
+    def compose(self) -> ComposeResult:
+        yield BorderTop()
+        yield ScreenBody()
+        yield BorderBottom()
