ea6422c161b1e5912bd793b6186e3f0f764e2986
Ross Murray <rossmurray@protonmail.com>
2023-08-16 16:52:12 +1000

    ref: move navigation widget into own modules

--- src/quandary/ui/components.py
+++ src/quandary/ui/components.py
@@ -10,23 +10,7 @@ from quandary.ui.widgets.SettingsTab import SettingsTab
 from quandary.ui.widgets.SwitchSetting import SwitchSetting
 from quandary.ui.widgets.DebugTab import DebugTab, DebugPanel
 from quandary.ui.widgets.ResponsePanel import ResponsePanel
-
-class TabbedNavigation(TabbedContent):
-    """The navigation pane"""
-    def compose(self) -> ComposeResult:
-        with TabbedContent("Response", "Debug", "Settings"):
-            yield ResponsePanel()
-            yield DebugTab()
-            yield SettingsTab()
-
-    def on_switch_setting_changed(self, event: SwitchSetting.Changed) -> None:
-        """when a SwitchSetting is toggled, log that to the debug pane."""
-        debug_panel = self.query_one(DebugPanel)
-        output = [{
-            "id": event.id,
-            "enabled": event.enabled
-        }]
-        debug_panel.append(output)
+from quandary.ui.widgets.NavigationTabs import NavigationTabs
 
 class InputPane(Static):
     """A widget to accept and send input"""
