abc37639df2343542748758d4f84db91cd3aaf5a
Ross Murray <rossmurray@protonmail.com>
2023-08-18 18:32:26 +1000

    feat: init profile selector component with mock inputs

--- src/quandary/ui/widgets/InputPanel.py
+++ src/quandary/ui/widgets/InputPanel.py
@@ -1,13 +1,22 @@
 from textual.app import ComposeResult
-from textual.widgets import Input, Static
+from textual.widgets import Input, Static, Select
 from textual.containers import Horizontal
+from textual.reactive import reactive
 from ..widgets import SwitchSetting
 
 class InputPanel(Static):
     """The user input text panel for submitting new questions."""
 
+    PROFILES = [
+       ( "one", 1),
+        ("two", 2),
+        ("three", 3),
+    ]
+    current_profile = reactive("two")
+
     def compose(self) -> ComposeResult:
         with Horizontal():
+            yield Select(self.PROFILES, prompt=self.current_profile, id="select_profile")
             yield Input(
                 id="text_input_field", placeholder="What do you want to know?"
             )
